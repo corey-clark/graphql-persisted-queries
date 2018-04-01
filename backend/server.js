a07671a9fd7ab1a514ec450ddebeb3b51b7bc583
corey.clark <corey.clark@ticketmaster.com>
2018-04-01 15:20:15 -0700

    update schema to schemas

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const { graphqlExpress, graphiqlExpress } = require('apollo-server-express')
 const { makeExecutableSchema } = require('graphql-tools')
 const playground = require('graphql-playground-middleware-express').default
 const persistedQueries = require('./persistedQueries')
-const typeDefs = require('./schema')
+const typeDefs = require('./schemas')
 const resolvers = require('./resolvers')
 
 const port = 4000
